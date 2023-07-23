35c1184a7062b1159dfb49d978d8cba9aa485ae4
Nelson A. Colón <nel.abdiel@gmail.com>
2023-07-23 17:02:24 -0400

    Updates

--- app/chatgphitAPP.py
+++ app/chatgphitAPP.py
@@ -5,10 +5,27 @@ import math
 import re
 import openai
 from dotenv import load_dotenv
+import psycopg2
+import tlsh
+
 
 load_dotenv()
 openai.api_key = os.environ.get('OPENAI_KEY')
 completion = openai.ChatCompletion()
+
+db_host = os.environ.get('HOST_DB')
+db_user = os.environ.get('USERNAME_DB')
+db_password = os.environ.get('PASSWORD_DB')
+db_name = os.environ.get('DB_DB')
+
+
+conn = psycopg2.connect(host=db_host, user=db_user, password=db_password, dbname=db_name)
+cur = conn.cursor()
+cur.execute('SELECT hash_value FROM document_hashes;')
+
+doc_hashes = cur.fetchall()
+#conn.commit()
+conn.close()
 
 
 def askgpt(question, chat_log=None):
@@ -54,9 +71,6 @@ def remove_pii(text):
 
 
 
-
-
-
 # Create the application instance
 APP = Flask(__name__)
 
@@ -73,17 +87,44 @@ def index():
     if request.method == 'GET':
         text = ''
         chatgpt = "Hi there, I will try my best to remove PII, PHI and IP but remember it's always better to not even include it in the first place."
-        #html = render_template('index.html')
-        #return html
+        
 
     else:
         if request.form:
-            #remove PII
-            text = remove_pii(request.form['input_text'])
-            # Get Answer
-            chatgpt= askgpt(text)[0]
-            #"Answer will appear here"
             
+            text = request.form['input_text']
+            
+            #text in bytes
+            btext = bytes(text, 'utf-8')
+            tlshash = tlsh.hash(btext)
+            
+            # suppose that documents are k = 800 apart.
+            k = 800
+            if len(doc_hashes) > 0:
+                for l in doc_hashes:
+                    d = tlsh.diff(l[0], tlshash)
+                    if d < k:
+                        k = d
+            #remove PII
+            text = remove_pii(text)
+
+            # suppose that documents are k = 800 apart.
+            k = 800
+            if len(doc_hashes) > 0:
+                for l in doc_hashes:
+                    d = tlsh.diff(l[0], tlshash)
+                    if d < k:
+                        k = d
+                        
+            # if the text is similar to something we know to be confidential then dont send it to chatgpt for an answer.
+            if k <= 100:
+                chatgpt = 'I apoligize, but this seems to be fairly similar to information I know to be Confidential'
+            
+            else:
+                # Get Answer
+                chatgpt= 'goof' #askgpt(text)[0]
+            
+            #"Answer will appear here"
             text = '~You~: ' + text
             
             
